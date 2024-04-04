44a518d46329dd15167b83512b9f6e07cb601d74
aweber1234 <158605438+aweber1234@users.noreply.github.com>
2024-04-04 13:24:34 -0400

    completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let count = 0;
+    let result = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    while (count < num) {
+        result += string;
+        count++;
+    }
+    return result;
 };
 
 // Do not edit below this line
