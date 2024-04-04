6bd19dc709a9d801a6ae8721045cc1b53fe8b848
aweber1234 <158605438+aweber1234@users.noreply.github.com>
2024-04-04 17:15:54 -0400

    completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let largeNum = 0;
+    let smallNum = 0;
+    let sum = 0;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) ||
+        num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+    if (num1 > num2) {
+        largeNum = num1;
+        smallNum = num2;
+    }
+    else {
+        smallNum = num1;
+        largeNum = num2;
+    }
+    for (let i = smallNum; i <= largeNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
